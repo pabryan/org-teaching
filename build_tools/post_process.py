936ec0e3e542081e7d321a9d07d552bcd426aa86
Paul Bryan <pabryan@gmail.com>
2024-09-26 15:58:04 +1000

    Initial setup imported from previous mq_math2020_2024_s2

---
+++ build_tools/post_process.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import sys
+import os
+import argparse
+
+from bs4 import BeautifulSoup
+import frontmatter # pip install python-frontmatter
+
+import pab_setup
+
+def process_note(soup: BeautifulSoup, contents: frontmatter.Post) -> str:
+    """Process a note"""
+
+    return convert_to_str(soup, contents, True)
+
+def process_subnote(soup: BeautifulSoup, contents: frontmatter.Post) -> str:
+    """Process a subnote"""
+
+    process_subnote_soup(soup)
+
+    return convert_to_str(soup, contents, True)
+
+def process_lecture(soup: BeautifulSoup, contents: frontmatter.Post) -> str:
+    """Process a lecture"""
+
+    return convert_to_str(soup, contents, True)
+
+def process_problem(soup: BeautifulSoup, contents: frontmatter.Post) -> str:
+    """Process a problem"""
+
+    return convert_to_str(soup, contents, True)
+
+def process_challenge(soup: BeautifulSoup, contents: frontmatter.Post) -> str:
+    """Process a challenge"""
+
+    return convert_to_str(soup, contents, True)
+
+types = {"n" : process_note, "s" : process_subnote, "l" : process_lecture, "p" : process_problem, "c" : process_challenge}
+
+def process_environments(soup: BeautifulSoup) -> BeautifulSoup:
+    # Convert theorem like environments
+    environment_specs = [{"type" : "thm", "title": "Theorem"},
+                         {"type" : "lem", "title": "Lemma"},
+                         {"type" : "defn", "title": "Definition"},
+                         {"type" : "eg", "title": "Example"},
+                         {"type" : "pf", "title": "Proof"}]
+
+    for environment_spec in environment_specs:
+        environments = soup.find_all("div", class_=environment_spec["type"])
+        environment_count = 0
+        for environment in environments:
+            environment_wrapper = soup.new_tag("div")
+            environment_wrapper["class"] = f"{environment_spec['type']} card"
+            environment_wrapper["id"] = f"{environment_spec['type']}-{environment_count}"
+
+            environment.wrap(environment_wrapper)
+            environment["class"] = "card-body"
+            del environment["id"]
+
+            card_header = soup.new_tag("div")
+            card_header["class"] = "card-header"
+
+            header = soup.new_tag("h5")
+            header["class"] = "card-title"
+            card_header.insert(0, header)
+
+            environment_wrapper.insert(0, card_header)
+
+            environment_count = environment_count + 1
+
+    return soup
+
+def process_subnote_soup(soup: BeautifulSoup) -> BeautifulSoup:
+    """Proccess a subnote soup"""
+
+    process_environments(soup)
+
+    return soup
+
+def convert_to_str(soup: BeautifulSoup, contents: frontmatter.Post, with_yaml: bool) -> str:
+    """Convert a soup to a string with or without YAML"""
+
+    soup.body.hidden = True
+    contents.content = soup.body.prettify()
+
+    if with_yaml:
+        output = frontmatter.dumps(contents)
+    else:
+        output = str(contents)
+
+    return output
+
+def process(source: str, destination: str, source_type: str):
+    """Process the input"""
+
+    with open(source, 'r') as f:
+        contents = frontmatter.load(f)
+
+    soup = BeautifulSoup(contents.content, "lxml")
+
+    output = types[source_type](soup, contents)
+
+    with open(args.destination, "w") as f:
+        f.write(output)
+
+def get_args() -> argparse.ArgumentParser:
+    """Get input arguments"""
+
+    parser = argparse.ArgumentParser(description="Post process org-exports")
+
+    parser.add_argument("-t", "--type", help="Type of org-export to convert", type=str, choices=types.keys(), required=True)
+    parser.add_argument("source", help="Source html file from custom org-export", type=str)
+    parser.add_argument("destination", help="Output html after processing", type=str)
+
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    os.chdir(os.path.join(sys.path[0], ".."))
+    #config = pab_setup.load_config("settings.json")
+
+    args = get_args()
+
+    process(args.source, args.destination, args.type)
---
+++ build_tools/setup.el
@@ -0,0 +1,217 @@
+(require 'yaml)
+(require 'json)
+(require 'org)
+(require 'ox)
+
+(make-local-variable 'pab/teaching-build-dir)
+(make-local-variable 'pab/teaching-export-dir)
+(make-local-variable 'pab/teaching-publish-dir)
+(make-local-variable 'pab/teaching-publish-dirs)
+
+(defun pab/teaching-load-settings ()
+  "Load settings file."
+
+  (let* ((json-object-type 'hash-table)
+	 (json-array-type 'list)
+	 (json-key-type 'string)
+	 (json-settings (json-read-file "settings.json")))
+
+    (setq pab/teaching-build-dir (gethash "build_dir" json-settings))
+    (setq pab/teaching-export-dir (file-name-concat pab/teaching-build-dir (gethash "export_dir" json-settings)))
+    (setq pab/teaching-publish-dir (file-name-concat pab/teaching-build-dir (gethash "publish_dir" json-settings)))
+    (setq pab/teaching-publish-dirs (gethash "publish_dirs" json-settings))))
+
+(defun pab/teaching-create-build ()
+  "Create build enviroment."
+  (interactive)
+  (make-directory pab/teaching-build-dir :parents)
+  (make-directory pab/teaching-export-dir :parents)
+  (make-directory pab/teaching-publish-dir :parents)
+  (maphash (lambda (hashkey hashval)
+	     (make-directory (file-name-concat pab/teaching-publish-dir hashval) :parents))
+	   pab/teaching-publish-dirs))
+
+(defun pab/teaching-export-to-backend (backends &optional outfile)
+  "Export at point using BACKENDS.
+BACKENDS should be a list of backends to use, e.g. ('html), ('html 'tex)
+Currently implemented are html and tex.
+If OUTFILE is set, export to that file in the pab/teaching-export-dir directory
+with appropriate file extension for echo backend in BACKENDS
+Otherwise let org decide the filename"
+
+
+  (dolist (backend backends)
+    (let ((ext (cond ((equal backend 'html) "html")
+		     ((equal backend 'latex) "tex")))
+	  (filename nil))
+      (if outfile
+	  (setq filename (file-name-concat pab/teaching-export-dir (format "%s.%s" outfile ext)))
+	(setq filename (org-export-output-file-name (format ".%s" ext) t pab/teaching-export-dir)))
+      (org-export-to-file backend filename nil t nil t))))
+
+(defun pab/teaching-export-file-name ()
+  "Construct the file-name for export.
+returns nil if EXPORT_FILE_NAME is already set
+otherwise builds filename as <week>_<lec>_<name>"
+
+  (unless (org-entry-get nil "EXPORT_FILE_NAME")
+    (let ((name (org-entry-get-with-inheritance "NAME"))
+	  (week (org-entry-get-with-inheritance "WEEK"))
+	  (lec (org-entry-get-with-inheritance "LECTURE")))
+      (format "%s_%s_%s" week lec name))))
+
+(defun pab/teaching-export-subtopic-file-name (filename_prefix)
+  "Construct the file-name for subtopic export.
+Accepts one argument FILENAME_PREFIX
+
+Returns nil if EXPORT_FILE_NAME is already set.
+Otherwise return FILENAME-CUSTOM_ID where CUSTOM_ID is obtained from the tags."
+
+  (unless (org-entry-get nil "EXPORT_FILE_NAME")
+    (let ((topic (org-entry-get-with-inheritance "CUSTOM_ID")))
+      (format "%s-%s" filename_prefix topic))))
+
+(defun pab/teaching-subnote-p ()
+  "Test if headline at point is a sub-note."
+
+  (save-excursion
+    (org-up-heading-safe)
+    (pab/teaching-note-p)))
+
+(defun pab/teaching-note-p ()
+  "Test if headline at point is a note."
+
+  (member "notes" (org-get-tags nil t)))
+
+(defun pab/teaching-lecture-p ()
+  "Test if headline at point is a lecture."
+
+  (member "lecture" (org-get-tags nil t)))
+
+(defun pab/teaching-problem-p ()
+  "Test if headline at point is a problem."
+
+  (member "problems" (org-get-tags nil t)))
+
+(defun pab/teaching-challenge-p ()
+  "Test if headline at point is a challenge."
+
+  (member "challenge" (org-get-tags nil t)))
+
+(defun pab/teaching-export-subnote (&optional filename)
+  "Export sub-topics.
+
+Saves to FILENAME if passed, otherwise let org decide the filename."
+
+  (if (pab/teaching-subnote-p)
+      (let ((export-filename (pab/teaching-export-subtopic-file-name filename)))
+	(pab/teaching-export-to-backend '(html) export-filename))
+    (message "Not a subnote")))
+
+(defun pab/teaching-get-notes-topics ()
+  "Get list of topics under current note."
+
+  (if (pab/teaching-note-p)
+      (let ((org-use-tag-inheritance nil))
+	(org-map-entries
+	 (lambda ()
+	   (format "\"%s\"" (org-entry-get nil "CUSTOM_ID")))
+	 "+notes-noexport" 'tree))
+    (message "Not a note")))
+
+(defun pab/teaching-export-note (&optional filename)
+  "Export a note.
+
+Saves to FILENAME if passed, otherwise let org decide the filename."
+
+  (if (pab/teaching-note-p)
+      (let ((note-level (org-element-property :level (org-element-at-point))))
+	(pab/teaching-export-to-backend '(latex html) filename)
+	(org-map-entries
+	 (lambda() (pab/teaching-export-subnote filename)) (format "LEVEL=%d" (+ note-level 1)) 'tree))
+    (message "Not a note")))
+
+(defun pab/teaching-export-lecture (&optional filename)
+  "Export a lecture.
+
+Saves to FILENAME if passed, otherwise let org decide the filename."
+
+  (if (pab/teaching-lecture-p)
+	(pab/teaching-export-to-backend '(html) filename)
+    (message "Not a lecture")))
+
+(defun pab/teaching-export-problems (&optional filename)
+  "Export problems.
+
+Saves to FILENAME if passed, otherwise let org decide the filename."
+
+  (if (pab/teaching-problem-p)
+      (pab/teaching-export-to-backend '(html latex) filename)
+    (message "Not a problem")))
+
+(defun pab/teaching-export-challenge (&optional filename)
+  "Export challenge.
+
+Saves to FILENAME if passed, otherwise let org decide the filename."
+
+  (if (pab/teaching-challenge-p)
+	(pab/teaching-export-to-backend '(html latex) filename)
+    (message "Not a challenge")))
+
+(defun pab/teaching-export ()
+  "Export headline based on tag.
+
+Possible tags are 'notes', 'lecture', 'problems', 'challenge'"
+
+  (interactive)
+  (let ((filename (pab/teaching-export-file-name)))
+    (cond ((pab/teaching-note-p)
+	   (pab/teaching-export-note (format "notes_%s" filename)))
+	  ((pab/teaching-subnote-p)
+	   (pab/teaching-export-subnote (format "notes_%s" filename)))
+	  ((pab/teaching-lecture-p)
+	   (pab/teaching-export-lecture (format "lecture_%s" filename)))
+	  ((pab/teaching-problem-p)
+	   (pab/teaching-export-problems (format "problems_%s"filename)))
+	  ((pab/teaching-challenge-p)
+	   (pab/teaching-export-challenge (format "challenge_%s"filename)))
+	  ('t (message "Can't export from here")))))
+
+(defun pab/teaching-export-all ()
+  "Export all notes, lectures and problems/challenges."
+
+  (interactive)
+  (let ((org-use-tag-inheritance nil))
+    (org-map-entries #'pab/teaching-export "+notes-noexport|+lecture-noexport|+problems-noexport|+challenge-noexport")))
+
+(defvar pab/teaching-mode-map (make-sparse-keymap))
+(define-key pab/teaching-mode-map (kbd "C-c e") #'pab/teaching-export)
+(define-key pab/teaching-mode-map (kbd "C-c x") #'pab/teaching-export-all)
+(define-key pab/teaching-mode-map (kbd "C-c m") (lambda () (pab/teaching-export-to-backend 'html)))
+(define-key pab/teaching-mode-map (kbd "C-c t") (lambda () (pab/teaching-export-to-backend 'latex)))
+
+(define-minor-mode pab/teaching-mode
+  "Minor mode for my teaching setup.
+
+Loads key-maps and loads settings."
+
+  :init-value nil
+  :lighter " pab/teaching"
+
+  (pab/teaching-load-settings)
+  (pab/teaching-create-build))
+
+;; Some testing to see how to loop through headlines matching tag without inheritance
+(defun pab/loop ()
+  "Loops through headlines."
+  (interactive)
+  (let ((org-use-tag-inheritance nil))
+    (org-map-entries #'pab/message "+lecture-noexport")))
+
+(defun pab/message ()
+  "Message at headline."
+  (interactive)
+
+  (let ((name (org-entry-get-with-inheritance "NAME"))
+	(tags (org-get-tags nil t)))
+    (message (format "%s: %s" name (nth 0 tags)))))
